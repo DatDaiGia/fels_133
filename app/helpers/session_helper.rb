70220e16c8b957d74e0a133dbfac6d48cccd558f
DatDaiGia <vandat2912@gmail.com>
2016-05-10 08:45:55 +0700

    Login + Log out

---
+++ app/helpers/session_helper.rb
@@ -0,0 +1,19 @@
+module SessionHelper
+
+  def log_in user
+    session[:user_id] = user.id
+  end
+
+  def current_user
+    @current_user ||= User.find_by id: session[:user_id]
+  end
+
+  def logged_in?
+    current_user.present? 
+  end
+
+  def log_out
+    current_user = nil
+    session.delete :user_id
+  end	
+end
